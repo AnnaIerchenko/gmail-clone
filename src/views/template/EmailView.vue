ec0e0b4882efcd856e03c772db10e26e6ff48cb9
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-05-19 13:35:05 -0700

    send message popup

--- src/views/template/EmailView.vue
+++ src/views/template/EmailView.vue
@@ -57,7 +57,9 @@
     </div>
     <div class="flex w-full justify-between">
       <div id="SideMenu" class="side-menu">
-        <div class="flex items-center justify-center bg-sky-200 w-36 h-8 mt-2 rounded-2xl ml-2 p-7 cursor-pointer"
+        <div 
+          @click="newMessageOpen = !newMessageOpen"
+          class="flex items-center justify-center bg-sky-200 w-36 h-8 mt-2 rounded-2xl ml-2 p-7 cursor-pointer"
         >
         <PencilOutline :size="25" class="mr-4"/>
         <span class="sm">Compose</span>
@@ -110,7 +112,7 @@
        <!-- here page chage -->
       <div class="m-4">
         <div class="w-6 h-6 flex justify-center mb-7">
-          <img class="object-center" src="img/GoogleCalandar.png" alt="">
+          <img class="object-center" src="img/GoogleCalendar.png" alt="">
         </div>
         <div class="w-6 h-6 flex justify-center mb-7">
           <img class="object-center" src="img/GoogleKeep.png" alt="">
@@ -129,10 +131,51 @@
         </div>
       </div>
     </div>
+
+    <!--send message popup -->
+    <div 
+      v-if="newMessageOpen"
+      id="NewMessageSection"
+      class="absolute bottom-0 right-0 mr-20 rounded-t-lg shadow-2xl bg-white">
+      <div class="flex items-center justify-between rounded-t-lg w-full text-sm px-3.5 py-2.5 bg-gray-200">
+        <div>New Message</div>
+        <Close 
+          @click="newMessageOpen = false"
+          class="cursor-pointer" :size="19" />
+      </div>
+
+      <div>
+        <div class="relative flex items-center px-3.5 py-2">
+          <div class="text-sm text-gray-700">To</div>
+          <input class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text"/>
+          <div class="absolute border-b w-[calc(100%-30px)] bottom-8"></div>
+        </div>
+
+        <div class="relative flex items-center px-3.5 py-2">
+          <div class="text-sm text-gray-700">Subject</div>
+          <input class="w-full h-6 border-transparent border-none focus:ring-0 outline-none" type="text"/>
+          <div class="absolute border-b w-[calc(100%-30px)] bottom-8"></div>
+        </div>
+      </div>
+
+      <div class="m-3">
+        <textarea 
+          style="resize:none"
+          class="w-full border-transparent border-none focus:ring-0 outline-none"
+          rows="14"></textarea>
+      </div>
+
+      <div class="p-4 mt-5">
+        <button
+          class="bg-blue-700 hover:bg-blue-600 text-white text-sm font-bold py-2 px-4 rounded-full"
+        >Send Message</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import {ref} from 'vue'
 import IconComponent from '@/components/IconComponent.vue';
 import UserComponent from '@/components/UserComponent.vue';
 import PencilOutline from 'vue-material-design-icons/PencilOutline.vue'
@@ -142,6 +185,9 @@ import ClockOutline from 'vue-material-design-icons/ClockOutline.vue'
 import SendOutline from 'vue-material-design-icons/SendOutline.vue'
 import FileOutline from 'vue-material-design-icons/FileOutline.vue'
 import Plus from 'vue-material-design-icons/Plus.vue'
+import Close from 'vue-material-design-icons/Close.vue'
+
+let newMessageOpen = ref(false)
 </script>
 
 <style lang="scss">
@@ -157,6 +203,10 @@ import Plus from 'vue-material-design-icons/Plus.vue'
     }
     .side-menu-item {
       width: 250px;
-    }
+    } 
+  }
+  #NewMessageSection {
+    width: 560px;
+    height: 570px;
   }
 </style>
