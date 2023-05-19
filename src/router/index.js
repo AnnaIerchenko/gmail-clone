25a43000ed0566757634c92ce701496598afd946
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-05-19 10:36:25 -0700

    top section

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import EmailView from '../views/template/EmailView.vue'
+const routes = [
+  {
+    path: '/email',
+    component: EmailView,
+    children: [
+      {
+        path: '',
+        component: HomeView
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="EmailView" class="bg-gray-50 h-screen">
+    <div class="flex items-center p-2">
+      <div class="logo-section flex justify-start items-center px-3.5">
+        <IconComponent
+          iconString="menu"
+          iconColor="#636363"
+          :iconSize="19"
+          hoverColor="hover:bg-gray-200"
+          text="Main menu"
+          class="-ml-2 mr-2"
+        />
+
+        <img width="107" src="../../../public/img/GmailLogo.png" alt="logo">
+      </div>
+      <div class="flex w-full justify-between">
+        <div class="input-width w-full bg-gray-200 flex items-center p-1 px-2 rounded-lg">
+          <IconComponent 
+            iconString="magnify"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Search"   
+          />
+          <input 
+            placeholder="Search mail"
+            class="w-full h-10 bg-gray-200 border-transparent border-none focus:ring-0 outline-none placeholder-gray-900"
+            type="text"
+          />
+          <IconComponent 
+            iconString="tune"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Show search options"   
+          />
+        </div>
+        <div class="flex w-32 justify-between items-center ml-6">
+          <IconComponent 
+            iconString="cog"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Settings"   
+          />
+          <IconComponent 
+            iconString="apps"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Google Apps"   
+          />
+
+          <UserComponent />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from '@/components/IconComponent.vue';
+import UserComponent from '@/components/UserComponent.vue';
+</script>
+
+<style lang="scss">
+  #EmailView {
+    .logo-section {
+      min-width: 250px; 
+    }
+    .input-width {
+      max-width: 700px;
+    }
+  }
+</style>
