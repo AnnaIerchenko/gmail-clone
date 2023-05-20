cac605bd409f14acbf3f360434ec345931a5a7ce
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-05-20 10:57:30 -0700

    google Auth Login with Node JS and Pinia

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+import vue3GoogleLogin from 'vue3-google-login'
+
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.use(vue3GoogleLogin,{
+  clientId: '252095222718-43pntekt91ur6t14gfbpbqo8ifhtm4s3.apps.googleusercontent.com'
+})
+app.mount('#app')
